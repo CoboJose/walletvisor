90c16873853a418b7c0e80263f2a76116675b799
Jose Cobo <cobogue@gmail.com>
2021-05-07 03:02:14 +0200

    cover

--- server/tests/test.bat
+++ server/tests/test.bat
@@ -1,8 +1,10 @@
 @ECHO OFF
 go test -coverpkg "../..." -coverprofile cover.out
-go tool cover -func cover.out
-go tool cover -html cover.out
+::go tool cover -func cover.out
+::go tool cover -html cover.out
 gocover-cobertura < cover.out > coverage.xml
+set CODACY_PROJECT_TOKEN=4993d6e6b495492099576b08364620eb
+curl -Ls https://coverage.codacy.com/get.sh report -r coverage.xml
 del cover.out
 PAUSE
 
