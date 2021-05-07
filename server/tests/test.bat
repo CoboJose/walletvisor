fe53eb626948c04ecce56d25e1717f3a31ec500f
Jose Cobo <cobogue@gmail.com>
2021-05-07 04:07:43 +0200

    coverage

--- server/tests/test.bat
+++ server/tests/test.bat
@@ -1,11 +1,7 @@
 @ECHO OFF
 go test -coverpkg "../..." -coverprofile cover.out
-::go tool cover -func cover.out
+go tool cover -func cover.out
 ::go tool cover -html cover.out
-::gocover-cobertura < cover.out > coverage.xml
-
-godacov -t 4993d6e6b495492099576b08364620eb -r ./cover.out -c c233e24c6912d5f57f1fc81f72c13cebbaaaaf1a
-
 del cover.out
 PAUSE
 
